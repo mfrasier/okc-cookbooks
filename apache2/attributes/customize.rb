0375f5b4e0ec36d472df0cc36b7bead22e3f5af0
marty frasier <morty.frasier@gmail.com>
2014-07-08 21:34:19 -0400

    added shadow apache2 cookbook

---
+++ apache2/attributes/customize.rb
@@ -0,0 +1,6 @@
+###
+# This is the place to override the apache2 cookbook's default attributes.
+# @see https://github.com/aws/opsworks-cookbooks/blob/release-chef-11.10/apache2/attributes/apache.rb
+###
+
+normal[:apache][:contact] = 'admin@okcra.org'
